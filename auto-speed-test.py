4f87edd3e6e73392487d76944a332e6a5541f5e8
James Fowkes <jamesfowkes@gmail.com>
2016-07-17 01:23:51 +0100

    Added apologetic comment

--- auto-speed-test.py
+++ auto-speed-test.py
@@ -9,6 +9,16 @@ class wait_for_testing_complete(object):
         pass
 
     def __call__(self, driver):
+        """
+        I feel I should explain the horror here.
+        In order to detect when the speed test has completed, a change needs to be detected on the page.
+        Since the test results are shown in a flash window (why, BT, why FLASH?), there's no way to detect that,
+        Instead, we look for the correct HTML for the navigation banner at the top of the page.
+        The "Results" tab changes colour to green when complete. Each tab is actually just an image, so
+        look for the correct img tags in the innerHTML of the headtry element.
+
+        I'm so very, very sorry.
+        """
         expected_inner_html = "<img src=\"images/home_grey.JPG\" alt=\"\" width=\"78\" height=\"25\" border=\"0\"><img src=\"images/testing_test.JPG\" alt=\"\" width=\"78\" height=\"25\" border=\"0\"><img src=\"images/results_gr.JPG\" alt=\"\" width=\"78\" height=\"25\" border=\"0\">"
         actual_inner_html = driver.find_element_by_id("headtry").get_attribute("innerHTML")
         return actual_inner_html == expected_inner_html
